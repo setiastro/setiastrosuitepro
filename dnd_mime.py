173398467fb6ad9296934d7d7ac9808bc0474ca8
setiastro <167908453+setiastro@users.noreply.github.com>
2025-10-10 19:48:57 -0500

    Delete dnd_mime.py

--- dnd_mime.py
+++
@@ -1,6 +0,0 @@
-# pro/dnd_mime.py
-MIME_VIEWSTATE = "application/x-sas-viewstate"
-MIME_CMD       = "application/x-sas-command"
-MIME_MASK      = "application/x-sas-mask"
-MIME_ACTION = "application/x-saspro-action-id"
-MIME_ASTROMETRY = "application/x-sas-astrometry"
