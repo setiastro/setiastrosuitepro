c3df3855143776dfddb44b9148fa2065d23d5710
setiastro <167908453+setiastro@users.noreply.github.com>
2025-10-10 19:42:10 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# Re-export a couple of things so `from pro import DocManager` works if you want.
-from .doc_manager import DocManager, ImageDocument
-from .subwindow import ImageSubWindow
-
-__all__ = ["DocManager", "ImageDocument", "ImageSubWindow"]
