4cb9c0ff7c9542edc9fd4fd9584a1b51b02b8727
setiastro <167908453+setiastro@users.noreply.github.com>
2025-10-14 14:36:14 -0500

    Add files via upload

--- legacy/image_manager.py
+++ legacy/image_manager.py
@@ -671,11 +671,11 @@ def load_image(filename, max_retries=3, wait_seconds=3):
                     # ---------------------------------------------------------------------
                     image = np.squeeze(image)
 
-                    if image.dtype == np.float32:
-                        max_val = image.max()
-                        if max_val > 1.0:
-                            print(f"Detected float image with max value {max_val:.3f} > 1.0; rescales to [0,1]")
-                            image = image / max_val
+                    #if image.dtype == np.float32:
+                    #    max_val = image.max()
+                    #    if max_val > 1.0:
+                    #        print(f"Detected float image with max value {max_val:.3f} > 1.0; rescales to [0,1]")
+                    #        image = image / max_val
                     # ---------------------------------------------------------------------
                     # 3) Interpret final shape to decide if mono or color
                     # ---------------------------------------------------------------------
@@ -719,11 +719,11 @@ def load_image(filename, max_retries=3, wait_seconds=3):
                 else:
                     raise ValueError("Unsupported TIFF format!")
 
-                if image.dtype == np.float32:
-                    max_val = image.max()
-                    if max_val > 1.0:
-                        print(f"Detected float image with max value {max_val:.3f} > 1.0; rescales to [0,1]")
-                        image = image / max_val
+                #if image.dtype == np.float32:
+                #    max_val = image.max()
+                #    if max_val > 1.0:
+                #        print(f"Detected float image with max value {max_val:.3f} > 1.0; rescales to [0,1]")
+                #        image = image / max_val
 
                 # Handle mono or RGB TIFFs
                 if image_data.ndim == 2:  # Mono
