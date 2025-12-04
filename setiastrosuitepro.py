7346e1f9e9adcacf9276cc558c091601ef42eca1
setiastro <167908453+setiastro@users.noreply.github.com>
2025-12-04 08:51:58 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- setiastrosuitepro.py
+++ setiastrosuitepro.py
@@ -420,7 +420,7 @@ except Exception:
     BUILD_TIMESTAMP = "dev"
 
 
-VERSION = "1.5.6"
+VERSION = "1.5.7"
 
 _update_splash("Loading resources...", 50)
 
@@ -662,3 +662,4 @@ if __name__ == "__main__":
         msg.setStandardButtons(QMessageBox.StandardButton.Ok)
         msg.exec()
         sys.exit(1)
+
