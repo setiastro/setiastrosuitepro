dafedebb9938744900834785193f367e7ebac834
setiastro <frank@setiastro.com>
2026-01-20 14:36:22 -0600

    fixed setiastrosuite entry in toml

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-01-20T17:37:12Z"
-APP_VERSION = "1.7.3.post1"
+BUILD_TIMESTAMP = "2026-01-20T20:36:01Z"
+APP_VERSION = "1.7.3.post2"
