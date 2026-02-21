1f5007a25a32f3068e98ac4baa6cc6cdb91aad36
setiastro <frank@setiastro.com>
2026-02-21 07:17:24 -0600

    CC and SPCC updates

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-02-20T20:51:41Z"
-APP_VERSION = "1.10.5.post1"
+BUILD_TIMESTAMP = "2026-02-21T13:16:57Z"
+APP_VERSION = "1.10.6"
