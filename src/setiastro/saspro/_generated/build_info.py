bec8aa39d38b2e1b96b52fe9fd01a4096758dab6
setiastro <frank@setiastro.com>
2026-03-01 13:54:41 -0600

    massive speed in CC engines

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-03-01T15:33:03Z"
-APP_VERSION = "1.11.0.post1"
+BUILD_TIMESTAMP = "2026-03-01T19:54:23Z"
+APP_VERSION = "1.11.1"
