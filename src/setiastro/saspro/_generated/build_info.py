cd542519560eebec8ba404cb6f3457d2cbdf6de4
setiastro <frank@setiastro.com>
2026-03-01 09:33:16 -0600

    post1 denoise engine

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-02-28T17:15:20Z"
-APP_VERSION = "1.11.0"
+BUILD_TIMESTAMP = "2026-03-01T15:33:03Z"
+APP_VERSION = "1.11.0.post1"
