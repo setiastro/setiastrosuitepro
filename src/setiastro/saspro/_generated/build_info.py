6e62998fb717fd3cfee115f97abfdc1469c6fda5
setiastro <frank@setiastro.com>
2026-03-26 12:33:16 -0500

    post2 mfd ram considerations

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-03-26T16:33:31Z"
-APP_VERSION = "1.13.2.post1"
+BUILD_TIMESTAMP = "2026-03-26T17:10:12Z"
+APP_VERSION = "1.13.2.post2"
