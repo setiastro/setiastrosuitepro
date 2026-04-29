b0dee980eeeb2010610c2596415445ea8a9bb8f9
setiastro <frank@setiastro.com>
2026-04-29 07:27:48 -0500

    increment version number

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-04-28T15:48:04Z"
-APP_VERSION = "1.15.7.post1"
+BUILD_TIMESTAMP = "2026-04-29T12:27:34Z"
+APP_VERSION = "1.15.8"
