0d3018def61cf887a020f1e74dff9bb1af2275fe
setiastro <frank@setiastro.com>
2026-05-18 07:18:01 -0500

    scikit and sigma lines

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-05-17T23:47:09Z"
-APP_VERSION = "1.16.4.post1"
+BUILD_TIMESTAMP = "2026-05-18T12:17:46Z"
+APP_VERSION = "1.16.4.post2"
