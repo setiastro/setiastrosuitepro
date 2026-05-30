6d5eb91bc8839e86cafbdcb9f4990154c3a1a7d0
setiastro <frank@setiastro.com>
2026-05-30 09:46:11 -0500

    fix fi.get error

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-05-30T13:05:30Z"
-APP_VERSION = "1.16.14"
+BUILD_TIMESTAMP = "2026-05-30T14:44:14Z"
+APP_VERSION = "1.16.14.post1"
