6f466c4bb455aab3bff867d7c773c0c81b3b1c38
setiastro <frank@setiastro.com>
2026-06-15 12:26:01 -0500

    minimize shelf error fix

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-06-15T00:42:01Z"
-APP_VERSION = "1.18.4.post2"
+BUILD_TIMESTAMP = "2026-06-15T17:25:50Z"
+APP_VERSION = "1.18.5"
