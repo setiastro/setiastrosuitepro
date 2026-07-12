f0d5b312b3050867c608c851397a56eb4ed272eb
setiastro <frank@setiastro.com>
2026-07-12 11:05:02 -0500

    neighborhood explorer

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-07-12T12:45:55Z"
-APP_VERSION = "1.19.4.post2"
+BUILD_TIMESTAMP = "2026-07-12T16:04:47Z"
+APP_VERSION = "1.19.5"
