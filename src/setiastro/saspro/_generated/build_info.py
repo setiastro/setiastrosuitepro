04121b3b86fa988b73e00d127d27ba081b4861a1
setiastro <frank@setiastro.com>
2026-08-09 12:45:35 -0500

    post1

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-08-09T12:47:50Z"
-APP_VERSION = "1.20.3"
+BUILD_TIMESTAMP = "2026-08-09T17:19:41Z"
+APP_VERSION = "1.20.3.post1"
