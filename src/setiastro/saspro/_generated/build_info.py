e860c3771bb479af0f49204569d1841e015abb2e
setiastro <frank@setiastro.com>
2026-08-25 08:44:57 -0500

    banding blaster and resource monitor for apple

--- src/setiastro/saspro/_generated/build_info.py
+++ src/setiastro/saspro/_generated/build_info.py
@@ -1,3 +1,3 @@
 # Auto-generated at build time. Do not edit.
-BUILD_TIMESTAMP = "2026-08-24T20:55:27Z"
-APP_VERSION = "1.20.8.post4"
+BUILD_TIMESTAMP = "2026-08-25T02:04:49Z"
+APP_VERSION = "1.20.9"
